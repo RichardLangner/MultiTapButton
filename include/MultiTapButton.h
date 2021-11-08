2cb2d09e38c2e38a9c84168e284e8b379bb44c19
richardlangner <richrdlangner.com>
2021-11-08 18:55:02 +0000

    Initial working code

---
+++ include/MultiTapButton.h
@@ -0,0 +1,179 @@
+/*
+MultiTapButton Class v0.11 updated 9 November 2021.
+Written by Richard Langner, Sheffield Hackspace, UK. 
+ 
+At its simplest, MultiTapButton debounces a physical button.
+MultiTapButton does not use interrupts so it can be used on
+just about any GPIO pin of your microprocessor board, and will
+not upset interrupt timing in ESP8266 boards.
+
+And there's plenty more functionality you can use.
+ 
+Use a MultiTapButton to detect:
+	SHORT PRESSES (TAPS)
+	- if you tapped the button				if(button1.tapped()){...}
+	- the number of taps					int x = button1.tapCount();
+	EVENTS
+	- if a button just went 'down'			if(button1.downEvent()){...}
+	- if a button just went 'up'			if(button1.upEvent()){...}
+	LONGER PRESSES
+	- duration of press	so far				unsigned long x = button1.downMillis();
+	- if a button down						if(button1.down()){...}
+	- if button is still down after (ms)	if(button1.downMillis(1000)){...}
+
+Example code is provided in 'MultiTapButton' sketch.
+
+-------------- USAGE --------------
+SIMPLE: If you have 'button1' which pulls GPIO2 LOW when pressed -
+	MultiTapButton button1(2,LOW);
+
+NOISY ENVIRONMENTS: As above, but increase the debounce time to 20ms -
+	MultiTapButton button1(2, LOW, 20)
+
+ADVANCED: Button has active HIGH pin GPIO2 debounced for 20ms -
+with tap maximum of 400ms and inter-tap gap of 200ms.
+	MultiTapButton button1(2, HIGH, 20, 400, 200);
+
+-------------- DEFAULTS --------------
+Debounce time 10ms.
+Maximum tap period 500ms, after which it's a long press.
+Maximum gap between taps for multi-tap 250ms.
+
+-------------- REQUIREMENTS --------------
+Accuracy depends on how often you call any of the button's functions, so ensure
+you have a fast non-blocking loop(); and check buttons regularly.
+
+-------------- EXTRAS --------------
+ For ease of coding, you can use each button's inbuilt general purpose user variables.
+ E.G.use integers as counters, and booleans for toggling things on/off, unsigned
+ longs for large numbers such as millis();
+	button1.userIntA				Spare Integer, free for you to use.
+	button1.userIntB				Spare Integer, free for you to use.
+	button1.userBoolA				Spare Boolean, free for you to use.
+	button1.userBoolB				Spare Boolean, free for you to use.
+    button1.userULongA;				Spare Unsigned Long,free for you to use.
+    button1.userULongB;				Spare Unsigned Long,free for you to use. 
+*/
+
+#include <Arduino.h>
+#ifndef  MULTI_TAP_BUTTON_CLASS
+#define  MULTI_TAP_BUTTON_CLASS
+
+class MultiTapButton
+{
+private:
+	unsigned long	_lastTimeDown		= millis();
+	unsigned long	_lastTimeUp			= millis();
+	unsigned long	_lastTimeTapped		= millis();
+	unsigned long	_down_ms;
+	unsigned long	_up_ms;		
+
+	bool			_longTapEnded = false;
+	bool			_shortTapEnded = false;
+	bool			_down = false;	
+	bool			_downEvent=false;
+	bool			_upEvent=false;
+	bool			_tapped;
+	int				_tapCounter;
+	int				_tapCounter2;
+	uint8_t			_buttonPin;			// Switch pin
+	bool			_activeLevel = LOW;	// Pin goes low when pressed
+	unsigned long	_debounce;			// Min tap duration ms(debounce)
+	unsigned long	_tap_ms;			// Max tap duration ms, then longtap
+	unsigned long	_tapGap_ms;			// Inter-tap gap, for multi-tap sensing
+
+public:
+	int				userIntA;			// Free to use
+	int				userIntB;			// Free to use
+	bool			userBoolA;			// Free to use
+	bool			userBoolB;			// Free to use
+    unsigned long   userULongA;			// free to use
+    unsigned long   userULongB;			// free to use
+
+
+// Create a TapButton object
+MultiTapButton(int Button_Pin, bool ActiveLevel = LOW, unsigned long Debounce = 10, unsigned long Tap_msec = 500, unsigned long TapGap_ms =250){
+	_buttonPin = Button_Pin;
+	_activeLevel = ActiveLevel;
+	_debounce = Debounce;
+	_tap_ms = Tap_msec;
+	_tapGap_ms = TapGap_ms;
+	pinMode(Button_Pin, INPUT);
+	}
+
+unsigned long update() {
+	// Reads the switch pin and saves the results
+	if(digitalRead(_buttonPin) == _activeLevel) {		// Read the button level, invert if active HIGH
+														// Button is down
+		if((millis() - _lastTimeDown) > _debounce){		// Debounce the button
+			_lastTimeUp = millis();						// Reset the UP timer
+			if(!_down){									// Event - only just pressed
+				_down = true;							// Remember the button is down
+				_downEvent=true;						// Down event happened
+				_down_ms = millis();					// Store the time it went down
+				_longTapEnded = false;					// Reset the UP flag
+				_shortTapEnded = false;					// Button is down
+				return 1;								// Button DOWN event =1
+			}
+		}
+		return 0;										// Nothing to report =0
+	}	
+	else
+	{	// Button is up
+	if((millis() - _lastTimeUp) > _debounce){		    // Debounce the button
+		_lastTimeDown = millis();						// Reset the DOWN timer
+		if(_down){										// Event - only just released
+			_down = false;								// Remember the button is up
+			_upEvent=true;								// Up event happened
+			_up_ms = millis();							// Store the time it went up
+			
+			_longTapEnded = ((_up_ms - _down_ms) > _tap_ms);	// Long tap
+			if(_longTapEnded){return (_up_ms - _down_ms);}
+			
+			_shortTapEnded = ((_up_ms - _down_ms) <= _tap_ms);	// Short tap
+			if(_shortTapEnded){++_tapCounter;}			// Increment tap counter
+			_lastTimeTapped = millis();					// Remember tap time
+			_tapped=true;								// Set flag
+			return 0;
+			}				
+		}
+	}
+	return 0;											// Nothing to report =0
+}
+
+// Returns TRUE if the button has just gone down
+bool	downEvent(){update();
+		if(_downEvent){_downEvent=false; return true;}
+		return false;
+		}
+// Returns TRUE if the button has just been released
+bool	upEvent(){update();
+		if(_upEvent){_upEvent=false; return true;}
+		return false;
+		}
+// Retruns TRUE if the button was tapped once
+bool	tapped(){update();
+		if(_tapped){_tapped=false; return true;}
+		return false;
+		}
+
+// Returns the number of consecutive taps
+int		tapCount(){update();
+		if((millis() - _lastTimeTapped) > _tapGap_ms){	// If multiple taps ended
+			int i = _tapCounter;						// Save tap counter value
+			_tapCounter=0;								// Reset tap counter
+			return i;									// Return tap couter
+		} else {return 0;}								// Else still tapping
+		}		
+
+// Returns time in milliseconds that the button has been held down		
+unsigned long downMillis(){update();
+		return _down ? (millis() - _down_ms) : 0;
+		}
+
+bool	down(){ update();
+			return _down;
+		}
+};
+
+#endif	// MULTI_TAP_BUTTON_CLASS
---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+ /*
+Example code for the 'MultiTapButton' class which debounces switch/button presses and taps.
+It reports how many times you multi-tapped the button, and reports if the
+button is held for more than 2 secs.
+** Monitor the Serial port for messages of what it is doing **
+
+All button timings are customisable - see 'MultiTapButton_01.h' for the extended
+parameter list.
+
+Written by Richard Langner, Sheffield, UK, 22 August 2021.
+
+This example should work on Arduino-type boards/Sonoff/ESP8266 etc.
+as it doesn't use special interrupts or timers.
+
+USAGE OF THIS EXAMPLE CODE
+Try quickly tapping the button one or more times, or holding
+it down for a few seconds, and see the results in the Serial port window.
+*/
+
+#include <Arduino.h>
+#include "MultiTapButton.h"
+#define BUTTON_1_PIN  D3		// My Wemos D1 mini button
+#define BUILTIN_LED D4			// My Wemos D1 mini LED pin
+
+// Create a TapButton object with active LOW pin, 
+MultiTapButton button1(BUTTON_1_PIN,LOW);
+
+void setup() {
+	pinMode(BUTTON_1_PIN, INPUT);					// Set the switched pin as an input
+	pinMode(BUILTIN_LED, OUTPUT);					// Enable pull-up resistor
+	digitalWrite(BUILTIN_LED, HIGH);				// Set LED to OFF
+	Serial.begin(74880);							// ESP8266 native baud rate, please add
+													// monitor_speed = 74880
+													// to the platformio.ini file
+}
+
+void loop() {
+
+	if(button1.downEvent()){			// Button just pressed down.
+		Serial.printf("EVENT: Button down\n");
+		}
+	
+	if(button1.upEvent()){				// Button just released.
+		Serial.printf("EVENT: Button up\n");
+		}
+
+	if(button1.tapped()){				// *Every* time button is tapped.
+		Serial.printf("Tapped\n");}
+
+  	int x = button1.tapCount();
+  	if(x>0){							// Your multi-tap code here.
+		  Serial.printf("Tapped %d times\n",x);
+		  }
+
+	if(x==6){							// Exactly 6 quick taps.
+		Serial.printf("You tapped EXACTLY %d times!", x);
+	}
+	
+	if(button1.downMillis() > 2000){	// Pressed for more than 2 seconds
+		Serial.printf("Button has now been down for %10lu ms\r", button1.downMillis());
+		}
+
+	// Turn on LED if button is pressed down
+	digitalWrite(BUILTIN_LED, !button1.down());
+}
+
