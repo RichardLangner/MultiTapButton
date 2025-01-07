43aa450fd55583b4a0325d0945e17d8d4a982287
Richa <Richa@ADELLE>
2025-01-07 18:37:25 +0000

    Working code, but misses the cghanges sometimes. Use interrupts instead.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /*By Richard Langner, Sheffield, UK, 6 Jan 2025
 
-Example code for the 'MultiTapButton' class
+Example code using the 'MultiTapButton' class
 *************************************************
 *     ROTARY ENCODER without interrupts         *
 *     GPIO pin numbers are for Wemos D1 mini    *
@@ -9,69 +9,38 @@ Example code for the 'MultiTapButton' class
 
 Captures clicks and direction of a rotary encoder
 All pins have pull-ups enabled in software
-Pins CLK=D7  DAT=D6  SWITCH=D5
+Pins CLK=D7  DAT=D6  BUTTON=D5
 Results are sent to the serial port.
 */
 
 #include <Arduino.h>
 #include "MultiTapButton.h"
 
-// Set the button and the LED pin numbers for your microcontroller
-#define BUTTON_1_PIN  2		// Button is connected between D2 and Ground
-#define BUILTIN_LED 13		// Arduino Nano is pin 13
+// Set the button and the pin numbers for your microcontroller
+#define CLK 	D7		// Switch is connected between D7 and Ground
+#define DAT 	D6		// Switch is connected between D6 and Ground
+#define BUTTON	D6		// Switch is connected between D5 and Ground
 
-// Create a TapButton object with active LOW pin, 
-MultiTapButton button1(BUTTON_1_PIN,LOW);
+// Create 3 MultiTapButton objects with active LOW pins, 
+MultiTapButton clk(CLK,LOW),dat(DAT,LOW), button(BUTTON,LOW);
 
 void setup() {
-	pinMode(BUTTON_1_PIN, INPUT_PULLUP);	// Set the switched pin as an input with pullup
-	pinMode(BUILTIN_LED, OUTPUT);			// Define LED pin
-	digitalWrite(BUILTIN_LED, HIGH);		// Set LED to OFF
-	Serial.begin(9600);						// Start the Serial output
+	pinMode(CLK, 	INPUT_PULLUP);	// Set the switched pins as an input with pullup
+	pinMode(DAT, 	INPUT_PULLUP);
+	pinMode(BUTTON, INPUT_PULLUP);
+	Serial.begin(74880);			// Start the Serial output
 }
 
 void loop() {
 
-	// Button just pressed down.
-	if(button1.downEvent()){				
-		Serial.println("EVENT: Button down");
+	// Check if just changed.
+	if(clk.downEvent() or clk.upEvent()){	
+		if(dat.down()){
+		Serial.println("EVENT: right");
 		}
-
-	// Button just released.
-	if(button1.upEvent()){
-		Serial.println("EVENT: Button up");
+		else{
+			Serial.println("EVENT: left");
 		}
-
-	// Occurs *every* time button is tapped.
-	if(button1.tapped()){					
-		Serial.println("Tapped, toggle the LED");
-		digitalWrite(BUILTIN_LED, !digitalRead(BUILTIN_LED));
-		}
-
-  	int x = button1.tapCount();
-	// Your multi-tap code here.
-  	if(x>0){								
-		  Serial.print("Tapped ");
-		  Serial.print(x);
-		  Serial.println(" times");
-		  }
-
-	// Exactly 5 quick taps enable auto-repeat.
-	if(x==5){
-		Serial.println("You tapped EXACTLY 5 times and enabled autorepeat!");
-		button1.autoRepeatEnabled(true);
-	}
-
-	// If button pressed for more than 6 seconds, disable auto-repeat
-	// and show the seconds the button was held down.
-	if(button1.downMillis() > 6000ul){
-		button1.autoRepeatEnabled(false);
-		if(button1.autoRepeatEnabled()){
-			Serial.println("Auto-repeat is now disabled.");
-		}
-		Serial.print("Button has been pressed for ");
-		Serial.print(button1.downMillis()/1000);
-		Serial.println(" seconds.");
 	}
 }
 
